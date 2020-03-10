17b0bce2c3728e24c425988ed62c3e82d2bd6710
Yirule <2356758998@qq.com>
2020-03-10 23:59:55 +0800

    fix some bugs

--- WindowOpen.py
+++ WindowOpen.py
@@ -3,48 +3,53 @@
 # author 弈云
 
 import autolog as au
-import requests
-import re
-import webbrowser
 import tkinter as tk
 import tkinter.messagebox
 import os
 
 user_data = []
+subject = []
 
 
 def read_user_data():
     if os.path.exists(os.getcwd()+'\\data.txt') == False:
         f = open(os.getcwd() + '\\data.txt', "w")
-        f.write("123456\n"+"zhangsan\n"+"123456")
+        f.write("abc\n"+"123456\n"+"zhangsan\n"+"123459")
         f.close()
 
     f = open(os.getcwd()+"\\data.txt", 'r', encoding='utf-8')
+
     for data in f.readlines():
         if len(data) != 0:
             user_data.append(data.strip('\n'))
     f.close()
-
-
-def login(class_id, account, password):
-    user1 = au.User(class_id, account, password)
-    responseRes = requests.post(user1.postUrl, data=user1.postData, headers=user1.header)
-
-    if len(class_id) != 0 and len(class_id) != 0 and len(password) != 0:
-        if tkinter.messagebox.askyesno(title='提示', message='是否保存密码？'):
-            f = open(os.getcwd()+"\\data.txt", 'w', encoding='utf-8')
-            f.write(class_id + '\n' + account + '\n' + password)
-            f.close()
-
-        pattern = re.compile(r'gensee://6170.*3b0a')
-        url = re.findall(pattern, responseRes.text)
-        # print(url)
-        if len(url) != 0:
-            webbrowser.open(url[0], new=0, autoraise=True)
-        else:
-            tkinter.messagebox.showerror(title='警告', message='ID或密码错误')
+    if len(user_data) % 4 == 0:
+        subject_number = len(user_data)
+        for i in range(int(subject_number/4)):
+            subject.append(au.Subject(user_data[i*4], user_data[i*4+1], user_data[i*4+2], user_data[i*4+3]))
     else:
-        tkinter.messagebox.showerror(title = '警告', message='输入不能为空')
+        tkinter.messagebox.showerror(title='警告', message='数据损坏\n请删除data.txt并重启！')
+
+
+def add_subject(class_name, class_id, account, password, list1):
+    subject.append(au.Subject(class_name, class_id, account, password))
+    list1.insert('end', class_name)
+    f = open(os.getcwd() + '\\data.txt', "a")
+    f.write(class_name+"\n" + class_id+"\n" + account+"\n" + password)
+    f.close()
+
+def update(tx1, tx2, tx3, tx4,list1):
+    tx1.delete(0, 'end')
+    tx2.delete(0, 'end')
+    tx3.delete(0, 'end')
+    tx4.delete(0, 'end')
+
+    number = list(list1.curselection())
+
+    tx1.insert('end', subject[number[0]].class_name)
+    tx2.insert('end', subject[number[0]].class_id)
+    tx3.insert('end', subject[number[0]].account)
+    tx4.insert('end', subject[number[0]].password)
 
 
 def main():
@@ -56,35 +61,56 @@ def main():
     window.title('自动登陆器')
 
     # 第3步，设定窗口的大小(长 * 宽)
-    window.geometry('500x250')  # 这里的乘是小x
+    window.geometry('385x200')  # 这里的乘是小x
 
-    # 第5步，用户信息
-    tk.Label(window, text='Class ID:', font=('Arial', 14)).place(x=5, y=5)
-    tk.Label(window, text='Account:', font=('Arial', 14)).place(x=5, y=45)
-    tk.Label(window, text='Password:', font=('Arial', 14)).place(x=5, y=85)
+    # 输入信息
+    tk.Label(window, text='Class Name:', font=('Arial', 10)).place(x=50, y=5)
+    tk.Label(window, text='Class ID:', font=('Arial', 10)).place(x=50, y=35)
+    tk.Label(window, text='Account:', font=('Arial', 10)).place(x=50, y=60)
+    tk.Label(window, text='Password:', font=('Arial', 10)).place(x=50, y=85)
 
-    tk.Label(window, text='Author:Yirule', font=('Arial', 14)).place(x=380, y=220)
+    tk.Label(window, text='Author:Yirule', font=('Arial', 10)).place(x=300, y=170)
+    # 课程列表
+    lb = tk.Listbox(window, listvariable=None, width=4, height=6)
+    lb.place(x=5, y=5)
+    # lb.selection_set(0)
 
+    for sub in subject:
+        lb.insert('end', sub.class_name)
+    # 课程名称
+    var_class_name = tk.StringVar()
+    var_class_name.set(user_data[0])
+    entry_class_name = tk.Entry(window, textvariable=var_class_name, font=('Arial', 10))
+    entry_class_name.place(x=135, y=5)
     # 课程ID
     var_class_id = tk.StringVar()
-    var_class_id.set(user_data[0])
-    entry_class_id = tk.Entry(window, textvariable=var_class_id, font=('Arial', 14))
-    entry_class_id.place(x=120, y=5)
+    var_class_id.set(user_data[1])
+    entry_class_id = tk.Entry(window, textvariable=var_class_id, font=('Arial', 10))
+    entry_class_id.place(x=135, y=35)
     # 用户名
-
     var_account = tk.StringVar()
-    var_account.set(user_data[1])
-    entry_account = tk.Entry(window, textvariable=var_account, font=('Arial', 14))
-    entry_account.place(x=120, y=45)
+    var_account.set(user_data[2])
+    entry_account = tk.Entry(window, textvariable=var_account, font=('Arial', 10))
+    entry_account.place(x=135, y=60)
     # 用户密码
     var_password = tk.StringVar()
-    var_password.set(user_data[2])
-    entry_password = tk.Entry(window, textvariable=var_password, font=('Arial', 14))
-    entry_password.place(x=120, y=85)
+    var_password.set(user_data[3])
+    entry_password = tk.Entry(window, textvariable=var_password, font=('Arial', 10))
+    entry_password.place(x=135, y=85)
 
-    log = tk.Button(window, text='Login', font=('Arial', 12), width=10, height=3,
-                    command=lambda: login(entry_class_id.get(), entry_account.get(), entry_password.get()))
-    log.place(x=365, y=25)
+    log = tk.Button(window, text='Login', font=('Arial', 12), width=8, height=1,
+                    command=lambda: au.Subject.login(subject[lb.curselection()]))
+    log.place(x=290, y=5)
+
+    add = tk.Button(window, text='Add', font=('Arial', 12), width=8, height=1,
+                    command=lambda: add_subject(entry_class_name.get(), entry_class_id.get(), entry_account.get(),
+                                               entry_password.get(), lb))
+    add.place(x=290, y=50)
+    up = tk.Button(window, text='Update', font=('Arial', 12), width=8, height=1,command=lambda: update(
+        entry_class_name, entry_class_id,entry_account, entry_password, lb))
+    up.place(x=290, y=100)
+
+    # user_data[0] = subject[lb.curselection()].class_name
 
     # 第5步，主窗口循环显示
     window.mainloop()
--- autolog.py
+++ autolog.py
@@ -1,24 +1,50 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import requests
+import tkinter
+import os
+import re
+import webbrowser
+import tkinter.messagebox
 
 
-class User(object):
+class Subject(object):
 
-    def __init__(self, class_id, account, password):
+    def __init__(self, class_name, class_id, account, password):
+        self.class_name = class_name
         self.class_id = class_id
         self.account = account
         self.password = password
 
-        userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
         (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
 
         self.header = {
             # "origin": "http://jhgkqrz.gensee.com",
             "Referer": "http://jhgkqrz.gensee.com/training/site/s/" + self.class_id,
-            'User-Agent': userAgent,
+            'User-Agent': user_agent,
         }
         self.postUrl = "http://jhgkqrz.gensee.com/training/site/s/" + self.class_id
         self.postData = {
             "nickname": account,
             "token": password,
         }
+
+    def login(self):
+        response_res = requests.post(self.postUrl, data=self.postData, headers=self.header)
+
+        if len(self.class_name) != 0 and len(self.class_id) != 0 and \
+                len(self.account) != 0 and len(self.password) != 0:
+            if tkinter.messagebox.askyesno(title='提示', message='是否保存密码？'):
+                f = open(os.getcwd() + "\\data.txt", 'w', encoding='utf-8')
+                f.write(self.class_id + '\n' + self.account + '\n' + self.password)
+                f.close()
+
+            url = re.findall(re.compile(r'gensee://6170.*3b0a'), response_res.text)
+            # print(url)
+            if len(url) != 0:
+                webbrowser.open(url[0], new=0, autoraise=True)
+            else:
+                tkinter.messagebox.showerror(title='警告', message='ID或密码错误')
+        else:
+            tkinter.messagebox.showerror(title='警告', message='输入不能为空')
